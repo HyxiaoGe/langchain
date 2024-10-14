53e35bbf3c2f5cda8be7174fedfdcde8ddb5f991
HyxiaoGe <18889592303@163.com>
2024-10-14 21:56:25 +0800

    first commit

---
+++ src/model-io/prompt.mjs
@@ -0,0 +1,20 @@
+import { PromptTemplate } from "langchain/prompts";
+
+// const prompt = PromptTemplate.fromTemplate(`the {attribute} of {target}`);
+
+const prompt = new PromptTemplate({
+  template: `the {attribute} of {target}`,
+  inputVariables: ["attribute", "target"]
+});
+
+const init = async () => {
+  const part = await prompt.partial({
+    attribute: "name",
+  });
+  const res = await part.format({
+    target: "Hyxiao",
+  });
+  console.log(res);
+};
+
+init();
